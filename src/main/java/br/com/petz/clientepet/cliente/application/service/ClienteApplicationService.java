0632c72e5ed2883d07569ba566102d4aa5a973c6
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-06-29 14:49:23 -0300

    implementa fluxo repository, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -31,8 +31,9 @@ public class ClienteApplicationService implements ClienteService {
 	@Override
 	public List<ClienteListResponse> buscaTodosClientes() {
 		log.info("[inicial] ClienteApplicationService - buscaTodosClientes");
+		List<Cliente> clientes = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
-		return null;
+		return ClienteListResponse.converte(clientes);
 	}
 
 }
