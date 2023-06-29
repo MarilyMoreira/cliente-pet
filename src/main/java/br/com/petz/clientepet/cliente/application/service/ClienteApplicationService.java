39d18a58050ec16b11de30c159dc37f7c5409c88
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-06-29 14:04:19 -0300

    implementa fluxo service, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -1,7 +1,10 @@
 package br.com.petz.clientepet.cliente.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
@@ -17,12 +20,19 @@ public class ClienteApplicationService implements ClienteService {
 
 	@Override
 	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
-		log.info("[inicial] ClienteApplicationService-criaCliente");
+		log.info("[inicial] ClienteApplicationService - criaCliente");
 		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
-		log.info("[finaliza] ClienteApplicationService-criaCliente");
+		log.info("[finaliza] ClienteApplicationService - criaCliente");
 		return ClienteResponse.builder()
 				.idCliente(cliente.getIdCliente())
 				.build();
+	}
+
+	@Override
+	public List<ClienteListResponse> buscaTodosClientes() {
+		log.info("[inicial] ClienteApplicationService - buscaTodosClientes");
+		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
+		return null;
 	}
 
 }
