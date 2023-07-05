2fbf912f1057f24f44ed9da6760f16f27888c5ad
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-07-05 19:49:53 -0300

    implementa alteracao

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAlteracaoRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAlteracaoRequest.java
@@ -2,10 +2,7 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import java.time.LocalDate;
 
-import org.hibernate.validator.constraints.br.CPF;
-
 import br.com.petz.clientepet.cliente.domain.Sexo;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
@@ -14,13 +11,13 @@ import lombok.Value;
 public class ClienteAlteracaoRequest {
 	@NotBlank
 	private String nomeCompleto;
-	@NotBlank
+	@NotBlank	
 	private String celular;
 	private String telefone;
 	private Sexo sexo;
 	@NotNull
 	private LocalDate dataNascimento;
-	@NotNull
+	@NotNull	
 	private Boolean aceitaTermos;
 
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
@@ -52,6 +53,17 @@ public class ClienteApplicationService implements ClienteService {
 		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
 		clienteRepository.deletaCliente(cliente);
 		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
+	}
+
+	@Override
+	public void patchAlteraCliente(UUID idCliente, ClienteAlteracaoRequest clienteAlteracaoRequest) {
+		log.info("[inicial] ClienteApplicationService - patchAlteraCliente");
+		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
+		cliente.altera(clienteAlteracaoRequest);
+		clienteRepository.salva(cliente);
+		log.info("[finaliza] ClienteApplicationService - patchAlteraCliente");
+		
+		
 	}
 
 }
