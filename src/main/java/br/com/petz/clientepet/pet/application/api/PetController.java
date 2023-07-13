e4aae1bc180e682df360e8097f7f370209d1f760
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-07-13 17:16:41 -0300

    implementa fluxo da camada de API

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -34,4 +34,12 @@ public class PetController implements PetAPI {
 		return petsDoCliente;
 	}
 
+	@Override
+	public PetClienteDetalhadoResponse getPetsDoClienteComId(UUID idCliente, UUID idPet) {
+		log.info("[inicia] PetController - getPetsDoClienteComId");
+		log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);
+		log.info("[finaliza] PetController - getPetsDoClienteComId");
+		return null;
+	}
+
 }
