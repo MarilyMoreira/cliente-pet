d6caeb8ff5418758e7cf8e8d9b0ca7307f102d13
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-07-13 21:21:51 -0300

    implementa repository busca petPeloId

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
@@ -43,8 +43,10 @@ public class PetApplicationService implements PetService {
 	@Override
 	public PetClienteDetalhadoResponse buscaPetDoClienteComId(UUID idCliente, UUID idPet) {
 		log.info("[start] PetApplicationService - buscaPetDoClienteComId");
+		clienteService.buscaClienteAtravesId(idCliente);
+		Pet pet = petRepository.buscaPetPeloId(idPet);
 		log.info("[finish] PetApplicationService - buscaPetDoClienteComId");
-		return null;
+		return new PetClienteDetalhadoResponse(pet);
 	}
 
 }
